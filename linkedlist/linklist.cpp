f1ef4f2051a9fd4a36b5d9d0fa73137cda5c74d1
Mihir Bhasin <57122515+Mihir-Bhasin@users.noreply.github.com>
2022-01-25 19:39:37 +0000

    reverse a linked list in one pass.

--- linkedlist/linklist.cpp
+++ linkedlist/linklist.cpp
@@ -110,3 +110,19 @@ void printNthFromEnd(Node *head,int n){
 }
 cout<<second->data;
 }
+
+
+//Reverse a linked list.
+
+
+Node *reverse(Node *head){
+    Node *curr=head;
+    Node *prev=NULL;
+    while(curr!=NULL){
+        Node *next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+}
