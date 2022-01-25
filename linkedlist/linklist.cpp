dd67271412b9c59f3a8c76a742a8ba35eb4f32ba
Mihir Bhasin <57122515+Mihir-Bhasin@users.noreply.github.com>
2022-01-25 17:48:25 +0000

    Nth nth node from end.

--- linkedlist/linklist.cpp
+++ linkedlist/linklist.cpp
@@ -74,4 +74,39 @@ void printmiddle(Node *head){
   }
   cout<<slow->data;
 }
+// nth node from end of linked list
+// uisng 2 loops.
 
+void printNthFromEnd(Node * head,int n){
+    if(head==NULL)return;
+    Node *first=head;
+    for(int i=0;i<n;i++){
+        if(first==NULL)return;
+        first=first->next;
+    }
+    Node *second=head;
+    while(first!=NULL){
+        second=second->next;
+        first=first->next;
+    }
+    cout<<(second->data);
+}
+
+//using 2 pointer with one iteration.
+// nth node from end of linked list
+void printNthFromEnd(Node *head,int n){
+  if(head==NULL)
+   return;
+  Node *first =head;
+  for(int i=0;i<n;i++)
+  { if(first==NULL)
+    first=first->next;
+  }
+  Node *second=head;
+  while(first!=NULL)
+{
+  first=first->next;
+  second=second->next;
+}
+cout<<second->data;
+}
