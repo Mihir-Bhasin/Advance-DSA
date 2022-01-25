876539f16037a22ade7c4f758525bae0f48704b8
Mihir Bhasin <57122515+Mihir-Bhasin@users.noreply.github.com>
2022-01-25 12:21:59 +0000

    Insert in LL.

---
+++ linkedlist/linklist.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+//node for linkedlist
+
+//basic already done.
+class Node{
+  public:
+  Node *next;
+  int key;
+  Node(int k)
+  {
+    key=k;
+    next=NULL;
+
+  }
+};
+
+
+//Insert in a given position in a linked list
+
+Node *insertPostion(Node *head, int pos,int data)
+{
+ Node* temp = new Node(data);
+ if(pos==1)
+ {
+   temp->next=head;
+   return temp;
+ }
+ Node *curr=head;
+ for(int i=1;i<pos-2&&curr !=NULL;i++)
+ {
+   curr=curr->next;
+ }
+ if(curr==NULL)
+  return head;
+
+  temp->next=curr->next;
+  curr->next=temp;
+  return head;
+}
+
+//sorted insert insert according to sorting
+
+Node *sortedinsert(Node *head,int x){
+  Node *temp=new Node(x);
+  if(head==NULL)
+    return temp;
+
+    
+
+
+}
