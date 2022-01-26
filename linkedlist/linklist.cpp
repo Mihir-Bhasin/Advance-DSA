52ecf37a0e4c9a476c6d045ad888710f2533569b
Mihir Bhasin <57122515+Mihir-Bhasin@users.noreply.github.com>
2022-01-26 09:44:37 +0000

    reverse ll kth node.

--- linkedlist/linklist.cpp
+++ linkedlist/linklist.cpp
@@ -134,3 +134,23 @@ Node *recRevL(Node *curr,Node *prev){
     curr->next=prev;
     return recRevL(next,curr);
 }
+
+
+//reverse kth node
+
+Node *reverseK(Node *head,int k){
+    Node *curr=head,*next=NULL,*prev=NULL;
+    int count=0;
+    while(curr!=NULL&&count<k){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+        count++;
+    }
+    if(next!=NULL){
+        Node *rest_head=reverseK(next,k);
+        head->next=rest_head;
+    }
+    return prev;
+}
